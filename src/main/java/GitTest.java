a020bd8ce1efa6f15a9fa212aa9a520b97b3e115
erkanuney <uneykn@gmail.com>
2021-03-16 18:51:11 -0500

    Erkan

--- src/main/java/GitTest.java
+++ src/main/java/GitTest.java
@@ -3,6 +3,10 @@ public class GitTest {
 
     public static void main(String[] args) {
         System.out.println("100_000.00$");
+        System.out.println("10");
+        System.out.println("Erkan");
+
+
     }
 
 
